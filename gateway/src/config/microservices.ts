eebd7c2890498ad7d779978c1e280a6a273cebc8
Allan Scala <allanscala@gmail.com>
2022-10-31 22:32:32 -0300

    feat: (gateway) create server and microservices url's

---
+++ gateway/src/config/microservices.ts
@@ -0,0 +1,12 @@
+import "dotenv/config"
+
+export enum MICROSERVICE {
+    USER = "http://localhost:3001",
+    EXAM = "http://localhost:3002",
+    CONSULT = "http://localhost:3003",
+    PATIENT = "http://localhost:3004",
+    DOCTOR = "http://localhost:3005",
+    INSURANCE_MEDICAL = "http://localhost:3006",
+    EMAIL = "http://localhost:3007",
+    FINANCIAL = "http://localhost:3008"
+}
---
+++ gateway/src/server.ts
@@ -0,0 +1,38 @@
+import express, { Express } from "express";
+import "dotenv/config";
+import cors from "cors";
+import body from "body-parser";
+import proxy from "express-http-proxy";
+import { MICROSERVICE } from "./config/microservices";
+
+const app: Express = express();
+const PORT = parseInt(process.env.PORT) || 3000
+const whiteList = []
+const corsOptions = {
+    origin: (origin, callback) => {
+        if(!origin || whiteList.indexOf(origin) !== -1) {
+            callback(null, true);
+        }else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    credentials: true
+}
+
+app.use(cors(corsOptions));
+app.use(body.json());
+app.use(body.urlencoded({extended: false}));
+
+// Microservices redirections
+app.use("/user", proxy(MICROSERVICE.USER))
+app.use("/exam", proxy(MICROSERVICE.EXAM))
+app.use("/consult", proxy(MICROSERVICE.CONSULT))
+app.use("/patient", proxy(MICROSERVICE.PATIENT))
+app.use("/doctor", proxy(MICROSERVICE.DOCTOR))
+app.use("/insurance", proxy(MICROSERVICE.INSURANCE_MEDICAL))
+app.use("/email", proxy(MICROSERVICE.EMAIL))
+app.use("/financial", proxy(MICROSERVICE.FINANCIAL))
+
+app.listen(PORT, () => {
+    console.log("Gateway is running");
+})
