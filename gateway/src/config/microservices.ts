3bd4857999072e6ee07a4f5aa532372ac3c8aa1d
Allan Scala <allanscala@gmail.com>
2023-01-05 15:43:47 -0300

    feat: create docker container patient service

--- gateway/src/config/microservices.ts
+++ gateway/src/config/microservices.ts
@@ -2,7 +2,7 @@ export enum MICROSERVICE {
     USER = "http://172.17.0.1:3001",
     EXAM = "http://localhost:3002",
     CONSULT = "http://localhost:3003",
-    PATIENT = "http://localhost:3004",
+    PATIENT = "http://172.17.0.1:3004",
     DOCTOR = "http://localhost:3005",
     INSURANCE_MEDICAL = "http://localhost:3006",
     AUTHENTICATION = "http://172.17.0.1:3009"
